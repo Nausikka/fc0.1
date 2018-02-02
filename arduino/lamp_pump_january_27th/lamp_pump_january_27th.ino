4009286c25883cd5f1332f5300165ba00a6b1d90
Jana Grajciarova <jana@repositive.io>
2018-02-02 08:47:58 +0000

    fixing clicking pump by removing initial setup

--- arduino/lamp_pump_january_27th/lamp_pump_january_27th.ino
+++ arduino/lamp_pump_january_27th/lamp_pump_january_27th.ino
@@ -26,6 +26,8 @@ void setup() {
   // set up the LCD's number of columns and rows:
   lcd.begin(16, 2);
   rtc.begin();
+  Serial.print("Running:lamp_pump_jan_27.ino");
+
 
   ////The following lines can be uncommented to set the date and time
 
@@ -39,16 +41,16 @@ void setup() {
   pinMode(pmp2,OUTPUT);
 
 //pump is off from the start
-digitalWrite(pmp1,HIGH);
-digitalWrite(pmp2,HIGH);
+//digitalWrite(pmp1,HIGH);
+//digitalWrite(pmp2,HIGH);
 
 //set pin for the lamp
   pinMode(lamp1,OUTPUT);
   pinMode(lamp2,OUTPUT);
 
 //lamp is off from the start
-  digitalWrite(lamp1,HIGH);
-  digitalWrite(lamp2,HIGH);
+  digitalWrite(lamp1,LOW);
+  digitalWrite(lamp2,LOW);
 
   Wire.begin();
   Serial.begin(9600);
@@ -86,17 +88,9 @@ void loop() {
   Hor = t.hour;
   Min = t.min;
   Sec = t.sec;
- if( Hor == 22 &&  (Min == 27) && (Sec > 45) ) //Comparing the current time with the Alarm time
+ if( Hor == 8 && Min == 19 ) //Comparing the current time with the Alarm time
 
       {
-      lcd.clear();
-      
-      lcd.print("Water");
-      
-      lcd.setCursor(0,1);
-      
-      lcd.print("Watering: ");
-      lcd.print(millis() / 1000);
       
       Pump();
       }else {      
@@ -106,7 +100,7 @@ void loop() {
       
   }
 
-  if( Hor >= 8 &&  (Min >= 27) ) //Comparing the current time with the Alarm time
+  if( Hor >= 19 && Hor < 21) //Comparing the current time with the Alarm time
 
       {
       lcd.clear();
@@ -121,30 +115,22 @@ void loop() {
       Lamp();
       }else {      
 //swithch the lamp off again when the time pass  
-      digitalWrite(lamp1,HIGH);
-      digitalWrite(lamp2,HIGH);
+      digitalWrite(lamp1,LOW);
+      digitalWrite(lamp2,LOW);
       
   }
 }
 
-void Pump()
-{
+void Pump() {
+  digitalWrite(pmp1,LOW);
+  digitalWrite(pmp2,LOW);
 
-digitalWrite(pmp1,HIGH);
-digitalWrite(pmp2,HIGH);
-
-delay(500);
-
-digitalWrite(pmp1, LOW);
-digitalWrite(pmp2, LOW);
-
-delay(500);
 }
 
 void Lamp(){
 
-digitalWrite(lamp1, LOW);
-digitalWrite(lamp2, LOW);
+digitalWrite(lamp1, HIGH);
+digitalWrite(lamp2, HIGH);
 }
 
 
